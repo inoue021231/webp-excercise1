6910d9aa00b4f51301cf7beac9773a0057a724f4
inoue021231 <114901330+inoue021231@users.noreply.github.com>
2022-10-24 16:22:19 +0900

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+// Image switcher code
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/tennis.png') {
+    myImage.setAttribute ('src','images/sports_tennis_man.png');
+  } else {
+    myImage.setAttribute ('src','images/tennis.png');
+  }
+}
+
+// Personalized welcome message code
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Welcome, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Welcome, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
